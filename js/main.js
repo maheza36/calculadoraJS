35820ecae36ddf358017a5e5f3195365ca1ff329
Andrés Ortiz <candresortizm@gmail.com>
2016-09-30 21:21:27 -0500

    Operaciones multiplicación y suma

--- js/main.js
+++ js/main.js
@@ -4,17 +4,24 @@ function myFunction() {
 };
 
     var recibirvalores = function(){
-    var intnum1 = prompt("Ingrese su primer numero");
-    var intnum2 = prompt("Ingrese su segundo numero");
-    if (isNaN(intnum1) || isNaN(intnum2) || (intnum1 === "") || (intnum2 === ""))
-    {
-      console.log("no se ingreso un número valido");
-    }
-    else{
+
+      do{
+        var intnum1 = prompt("Ingrese su primer numero");
+        if(isNaN(intnum1) || intnum1 === ""){
+          console.log("no se ingreso un número valido");
+        }
+      }while(isNaN(intnum1) || intnum1 === "");
+
+      do{
+        var intnum2 = prompt("Ingrese su segundo numero");
+        if(isNaN(intnum2) || intnum2 === ""){
+          console.log("no se ingreso un número valido");
+        }
+      }while(isNaN(intnum2) || intnum2 === "");
+
       intnum1 = parseFloat(intnum1);
       intnum2 = parseFloat(intnum2);
       recibiroperador(intnum1,intnum2);
-    }
   };
 
   var recibiroperador=function(intnum1,intnum2){
@@ -23,33 +30,59 @@ function myFunction() {
   };
 
   var seleccionaroperacion = function(operador,intnum1,intnum2){
-    switch(operador) {
-      //restar
-      case "restar":
-      case "less":
-      case "LESS":
-      case 'RESTAR':
-      case '-':
-      case 'menos':
-      case 'MENOS':
-      case 'res':
-      case 'RES':
-      var resultado = restar(intnum1, intnum2);
+      switch(operador) {
+        //restar
+        case "restar":
+        case "less":
+        case "LESS":
+        case 'RESTAR':
+        case '-':
+        case 'menos':
+        case 'MENOS':
+        case 'res':
+        case 'RES':
+          var resultado = restar(intnum1, intnum2);
+        break;
 
-      break;
-      //dividir
-      case "dividir":
-      case "/":
-      case "divide":
-      case "DIVIDIR":
-      case "DIVIDE":
-      case "division":
-      case "DIVISION":
-      var resultado = dividir(intnum1,intnum2);
-      break;
-      default:
-      var resultado = "Signo no definido"
-    }
+        //dividir
+        case "dividir":
+        case "/":
+        case "divide":
+        case "DIVIDIR":
+        case "DIVIDE":
+        case "division":
+        case "DIVISION":
+          var resultado = dividir(intnum1,intnum2);
+        break;
+
+        //sumar
+        case "sumar":
+        case "add":
+        case "ADD":
+        case 'SUMAR':
+        case '+':
+        case 'mas':
+        case 'MAS':
+          var resultado = sumar(intnum1, intnum2);
+        break;
+
+        //multiplicar
+        case "multiplicar":
+        case "multiply":
+        case "MULTIPLY":
+        case 'MULTIPLICAR':
+        case '*':
+        case 'por':
+        case 'POR':
+        case 'mult':
+        case 'MULT':
+          var resultado = multiplicar(intnum1, intnum2);
+        break;
+
+        default:
+          var resultado = "Signo no definido";
+    }while(resultado === "Signo no definido");
+
     console.log(resultado);
     document.getElementById("demo").innerHTML = "El resultado es de " + intnum1 + " " + operador + " " + intnum2 + " es: " + resultado;
     //javascript:location.reload();
@@ -57,6 +90,7 @@ function myFunction() {
   };
 
   var restar = function(intv1,intv2){
+    operador = "-";
     return (parseFloat(intv1) - parseFloat(intv2));
   };
 
@@ -66,6 +100,17 @@ function myFunction() {
       return "No se puede dividir entre cero";
     }
     else {
+      operador = "/";
       return (parseFloat(intv1) / parseFloat(intv2));
     }
+  };
+
+  var sumar = function(intv1,intv2){
+    operador = "+";
+    return (parseFloat(intv1) + parseFloat(intv2));
+  };
+
+  var  multiplicar = function (intv1, intv2) {
+    operador = "*";
+    return (parseFloat(intv1) * parseFloat(intv2));
   };
