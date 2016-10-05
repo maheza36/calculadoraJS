d7699d5ecc0593ae75e7b82cc49f824fca717331
Mauricio Hernández <maheza36@gmail.com>
2016-10-04 21:57:59 -0500

    calculadora js 2

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,30 +1,60 @@
-calculator ={
-  op1 : null,
-  op2 : null,
+var calculator ={
+  res : null,
   operator : null,
   check_input : function(){
+    var self = calculator;
+    console.log("Check Input");
     var input = document.getElementById("input");
-    var input_text = input.textcontent;
+    var input_text = input.value;
     var errors = document.querySelector("#error");
     var input_number = Number(input_text);
-    if(input_number!== NaN){
-      if(this.op1 === null){
-        this.op1 = input_number;
-      }
-      else {
-        this.op2 = input_number;
-      }
+    if(Number.isNaN(input_number)){
+      input.value="";
     }
-  }
+  },
+  set_number: function(button){
+    return function(){
+      var input = document.getElementById("input");
+      input.value = "" + input.value  + button.textContent;
+    };
+  },
+  set_operator: function(button){
+    var self = calculator;
+    return function(){
+      if(self.res === null){
+        self.res = input.value;
+      }
+      self.operators = button.textContent;
+    };
+  },
+  clear: function(){
+    var self = calculator;
+    self.res=null;
+    self.operators = null;
+    var input = document.getElementById("input");
+    input.value ="";
+  },
 };
 
 document.addEventListener("DOMContentLoaded",function(event){
+  //una vez la página ha cargado
   var output = document.getElementById("output");
   var controlpad = document.getElementById("controlpad");
-  var buttons_operators = document.querySelectorAll("#operators button");
-  var i=0;
-  
+  var buttons_operators = controlpad.querySelectorAll("#operators button");
+  var i;
   for(i=0; i<buttons_operators.length;i++){
     buttons_operators[i].addEventListener("click",calculator.check_input);
   }
+  var button_numbers = controlpad.querySelectorAll("#numbers button");
+  for(i=0;i<button_numbers.length;i++){
+    console.log(button_numbers[i]);
+    button_numbers[i].addEventListener(
+      "click",
+      calculator.set_number(button_numbers[i])
+    );//cierra el for de button_numbers
+
+  }
+  var button_clear = controlpad.querySelector("#clear");
+  button_clear.addEventListener("click",calculator.clear);
+  console.log("Initialited");
 });
